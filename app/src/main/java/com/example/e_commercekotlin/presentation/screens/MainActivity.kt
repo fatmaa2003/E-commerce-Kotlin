c724a8b0d96f85472b424a0a46f211120513acea
Mahmoud <mahmoudali77009@gmail.com>
2024-08-12 16:35:57 +0300

    buttom navigation_bar

--- app/src/main/java/com/example/e_commercekotlin/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/example/e_commercekotlin/presentation/screens/MainActivity.kt
@@ -2,19 +2,42 @@ package com.example.e_commercekotlin.presentation.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.e_commercekotlin.R
+import com.example.e_commercekotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, LoginFragment())
-                .commit()
+        replaceFragment(FeedFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Feed -> replaceFragment(FeedFragment())
+                R.id.Market -> replaceFragment(MarketFragment())
+                R.id.Profile -> replaceFragment(ProfileFragment())
+            }
+            true
         }
+
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fragment_container, LoginFragment())
+//                .commit()
+//        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
-
