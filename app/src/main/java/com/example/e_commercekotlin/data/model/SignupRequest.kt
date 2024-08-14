7e7b0a7d63ae0ef1edf60619289283646afc4fad
Mahmoud <mahmoudali77009@gmail.com>
2024-08-14 12:16:42 +0300

    fix project errors

--- app/src/main/java/com/example/e_commercekotlin/data/model/SignupRequest.kt
+++ app/src/main/java/com/example/e_commercekotlin/data/model/SignupRequest.kt
@@ -7,13 +7,3 @@ data class SignupRequest(
     val password: String
 )
 
-package com.example.e_commercekotlin.data.model
-
-data class SignupRequest(
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val username: String,
-    val password: String
-)
-
