99d3b0d2cd028cb1f1f1794135f0a742a61e3adb
Mostafa Tmer <mostafat2002@gmail.com>
2024-08-18 15:29:36 +0300

    prepare market fragment

--- app/src/main/java/com/example/e_commercekotlin/presentation/screens/MarketFragment.kt
+++ app/src/main/java/com/example/e_commercekotlin/presentation/screens/MarketFragment.kt
@@ -1,17 +1,12 @@
 package com.example.e_commercekotlin.presentation.screens
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.e_commercekotlin.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MarketFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MarketFragment : Fragment() {
 
 
@@ -19,7 +14,12 @@ class MarketFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_market, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
