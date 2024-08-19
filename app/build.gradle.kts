cb6b98669df20a52da1a131bc2ce3be43881e1dc
Hanan <nny2003@hotmail.com>
2024-08-19 14:52:10 +0300

    - Implemented CategoryAdapter for displaying categories in a horizontal RecyclerView. - Implemented ProductAdapter (MyAdapter) for displaying products in a staggered grid layout. - Integrated Fake Store API for fetching categories and products. - Added data fetching logic in FeedFragment and updated UI with fetched data.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.gms:play-services-ads:23.2.0")
+    implementation ("com.squareup.picasso:picasso:2.8")
 }
