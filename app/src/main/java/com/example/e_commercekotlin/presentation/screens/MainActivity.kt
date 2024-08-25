66f86fd8a6d4c992c1fa324fd061a5755c3f8e63
fatma <fatmaashraf433@gmail.com>
2024-08-25 13:57:29 +0300

    finish featured

--- app/src/main/java/com/example/e_commercekotlin/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/example/e_commercekotlin/presentation/screens/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.e_commercekotlin.presentation.screens
 
-import CollectionDetails
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
