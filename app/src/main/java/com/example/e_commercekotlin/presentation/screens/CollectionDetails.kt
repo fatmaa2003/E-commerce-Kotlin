a1260e50e8b70c6e9031229a0a3cf29914c5c7c0
Mahmoud <mahmoudali77009@gmail.com>
2024-08-25 13:05:14 +0300

    update/collection/details feature

--- app/src/main/java/com/example/e_commercekotlin/presentation/screens/CollectionDetails.kt
+++ app/src/main/java/com/example/e_commercekotlin/presentation/screens/CollectionDetails.kt
@@ -24,16 +24,16 @@ class CollectionDetails : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // example lhad ma ne3ml integrat ma3 api
-        val horizontalData = listOf("clothes 1", "Item 2", "Item 3")
+        val horizontalData = listOf("clothes")
         val verticalData = listOf(
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 1"),
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 2"),
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 3"),
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 4"),
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 5"),
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 6"),
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 7"),
-            CollectionDetailsAdapter.Item(R.drawable.cat1, "Name 8")
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 1"),
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 2"),
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 3"),
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 4"),
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 5"),
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 6"),
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 7"),
+            CollectionDetailsAdapter.Item(R.drawable.levis, "Name 8")
         )
         binding.horizontalRecyclerView.layoutManager = LinearLayoutManager(
             requireContext(),
