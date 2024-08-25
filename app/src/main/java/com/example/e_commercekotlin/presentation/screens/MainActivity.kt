c9e2c9486ffb317b6f44aa503952fe2bbe0c5026
nada <dodzshaheen62@gmail.com>
2024-08-25 12:17:52 +0300

    brand page

--- app/src/main/java/com/example/e_commercekotlin/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/example/e_commercekotlin/presentation/screens/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.Feed -> replaceFragment(FeedFragment())
                 R.id.Market -> replaceFragment(CollectionFragment())
-                R.id.Profile -> replaceFragment(FeaturedFragment())
+                R.id.Profile -> replaceFragment(BrandFragment())
             }
             true
         }
