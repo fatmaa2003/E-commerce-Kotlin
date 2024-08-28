b129f1e148b0a53382d9d227b42cec7b72f3af69
nada <dodzshaheen62@gmail.com>
2024-08-28 13:36:06 +0300

    add card fragment

--- app/src/main/java/com/example/e_commercekotlin/presentation/screens/AddCard.java
+++ app/src/main/java/com/example/e_commercekotlin/presentation/screens/AddCard.java
@@ -11,38 +11,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.e_commercekotlin.R;
-import com.example.e_commercekotlin.data.model.Brand;
 import com.example.e_commercekotlin.data.model.CardModel;
-import com.example.e_commercekotlin.databinding.FragmentPaymentBinding;
-import com.example.e_commercekotlin.presentation.adapter.BrandAdapter;
+import com.example.e_commercekotlin.databinding.FragmentAddToCartBinding;
 import com.example.e_commercekotlin.presentation.adapter.PaymentAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PaymentFragment extends Fragment {
+public class AddCard extends Fragment {
     private PaymentAdapter paymentAdapter;
     private List<CardModel> cardModels= new ArrayList<>();
-    private FragmentPaymentBinding binding;
+    private FragmentAddToCartBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentPaymentBinding.inflate(inflater, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAddToCartBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         paymentAdapter = new PaymentAdapter();
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        binding.cardRecyclerView.setLayoutManager(layoutManager);
-        binding.cardRecyclerView.setAdapter(paymentAdapter);
+        binding.addToCartRecyclerview.setLayoutManager(layoutManager);
+        binding.addToCartRecyclerview.setAdapter(paymentAdapter);
         cardModels.add(new CardModel(CardModel.CardType.MASTER_CARD, "123988"));
         cardModels.add(new CardModel(CardModel.CardType.VISA, "18972"));
+        cardModels.add(new CardModel(CardModel.CardType.VISA, "12398"));
         cardModels.add(new CardModel(CardModel.CardType.VISA, "18972"));
-
+        cardModels.add(new CardModel(CardModel.CardType.VISA, "18972"));
+        cardModels.add(new CardModel(CardModel.CardType.VISA, "18972"));
         paymentAdapter.setCardModels(cardModels);
 
     }
