4d8d20606592c4a7edcd67afb9e414ad50fcd274
fatma <fatmaashraf433@gmail.com>
2024-09-18 16:17:17 +0300

    implementation of frequently product navigation to product details

--- app/src/main/java/com/example/e_commercekotlin/presentation/adapter/FrequentlyVisitedAdapter.kt
+++ app/src/main/java/com/example/e_commercekotlin/presentation/adapter/FrequentlyVisitedAdapter.kt
@@ -4,16 +4,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.e_commercekotlin.R
 import com.example.e_commercekotlin.data.model.ProductResponse
+import com.example.e_commercekotlin.presentation.adapter.ProductAdapter.ClickListener
 
 
 class FrequentlyVisitedAdapter() : RecyclerView.Adapter<FrequentlyVisitedAdapter.MyViewHolder>() {
 
     private var freqVisitedList : ProductResponse = ProductResponse()
+    lateinit var onProductClick : OnFrequentlyProductClick
+
+    fun setListener(onProductClick: OnFrequentlyProductClick) {
+        this.onProductClick = onProductClick
+    }
 
     fun setFrequentlyVisitedItemsList(freqVisitedList: ProductResponse){
         this.freqVisitedList = freqVisitedList
@@ -31,6 +38,18 @@ class FrequentlyVisitedAdapter() : RecyclerView.Adapter<FrequentlyVisitedAdapter
         holder.freqVisitedItemType.text = currentItem.productName
         holder.freqVisitedItemCategory.text = currentItem.price.toString()
         Glide.with(holder.freqVisitedItemImage.context).load(currentItem.imageUrl).into(holder.freqVisitedItemImage)
+
+        holder.item.setOnClickListener {
+            currentItem.let {
+                it.productId?.toLong()?.let { it1 ->
+                    onProductClick.onProductClick(
+                        productId = it1,
+                        productImage = currentItem.imageUrl.orEmpty() ,
+                        productName = currentItem.productName.orEmpty()
+                    )
+                }
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -41,5 +60,10 @@ class FrequentlyVisitedAdapter() : RecyclerView.Adapter<FrequentlyVisitedAdapter
         val freqVisitedItemImage: ImageView = itemView.findViewById(R.id.freq_visited_item_image)
         val freqVisitedItemType: TextView = itemView.findViewById(R.id.freq_visited_type_text)
         val freqVisitedItemCategory: TextView = itemView.findViewById(R.id.freq_visited_category_text)
+        val item : RelativeLayout = itemView.findViewById(R.id.layout)
+    }
+
+    interface OnFrequentlyProductClick{
+        fun onProductClick(productId: Long, productName: String, productImage: String)
     }
 }
